f15eb98192926c31e7fc5028ad179c6f6fff6d16
strNophix <nvdpoel01@gmail.com>
2022-10-20 19:46:10 +0200

    Updated message padding and text size

--- client/components/message/ChatMessage.tsx
+++ client/components/message/ChatMessage.tsx
@@ -9,7 +9,7 @@ const ChatMessage: FC<ChatMessageProps> = ({
   message: { fromUser, content },
 }) => {
   return (
-    <div className="mx-2 p-2 hover:bg-neutral-700 text-sm rounded-md">
+    <div className="mx-2 p-[0.4rem] hover:bg-neutral-700 text-xs rounded-md">
       <div className="space-x-1 inline">
         <span className="align-middle">{fromUser}: </span>
       </div>
